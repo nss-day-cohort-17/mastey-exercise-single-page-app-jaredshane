3c749d38dc4d25c56a65c49a80de8b87efbf7db9
Jared Fuller <jaredshane@gmail.com>
2016-12-13 15:59:34 -0600

    added functionality to loop through each div

--- quiz.js
+++ quiz.js
@@ -1,13 +1,38 @@
+var carData;
 var inventory;
-var data;
 var insertData = document.getElementById('insertData')
+var inventoryList = "";
 loadInventory();
+var targetCard;
+console.log(targetCard)
 
 function populatePage (inventory) {
   // Loop over the inventory and populate the page
+  for (var i = 0; i < carData.cars.length; i++) {
+    inventoryList = `
+                      <div class = "carCard col-xs-4">
+                        <div>Make: ${carData.cars[i].make}</div>
+                        <div>Model: ${carData.cars[i].model}</div>
+                        <div>Year: ${carData.cars[i].year}</div>
+                        <div>Year: ${carData.cars[i].price}</div>
+                        <div>Year: ${carData.cars[i].description}</div>
+                      </div>
+                      `
+    insertData.innerHTML += inventoryList;
 
+
+      }
+    targetCard = document.getElementById('insertData').getElementsByClassName('carCard')
+
+    for (var i = 0; i < targetCard.length; i++) {
+      console.log(targetCard[i])
+      targetCard[i].addEventListener('click', function (e) { console.dir(e.target.parentElement)
+
+        
+      })
+    }
   // Now that the DOM is loaded, establish all the event listeners needed
-  activateEvents();
+  // activateEvents();
 }
 
 // Load the inventory and send a callback function to be
@@ -15,23 +40,13 @@ function populatePage (inventory) {
 
 function loadInventory (callback) {
   var inventoryLoader = new XMLHttpRequest();
-  var inventoryList = "";
+  inventoryLoader.open("GET", "inventory.json");
+  inventoryLoader.send();
+
   inventoryLoader.addEventListener("load", function (e) {
-    data = JSON.parse(e.target.responseText);
-    console.log(data);
-    for (var i = 0; i < data.cars.length; i++) {
-      inventoryList = `<div class="col-md-3 carCard">
-                          <div>Make: ${data.cars[i].make}</div>
-                          <div>Model: ${data.cars[i].model}</div>
-                          <div>Year: ${data.cars[i].year}</div>
-                          <div>Year: ${data.cars[i].price}</div>
-                          <div>Year: ${data.cars[i].description}</div>
-                        </div>`
-      insertData.innerHTML += inventoryList;
-
-    }
-
+    carData = JSON.parse(e.target.responseText);
+    populatePage(carData);
   });
-  inventoryLoader.open("GET", "inventory.json")
-  inventoryLoader.send()
+
+
 }
